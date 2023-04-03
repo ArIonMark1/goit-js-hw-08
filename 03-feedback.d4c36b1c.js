08418203fe124dcc72ba1adfc1b970bfbf7caced
ArIonMark1 <65442269+ArIonMark1@users.noreply.github.com>
2023-04-03 17:10:36 +0000

    Deploying to gh-pages from @ ArIonMark1/goit-js-hw-08@44bd71b9c65a47609bafff9ad6f2d5ae5c76017d 🚀

---
+++ 03-feedback.d4c36b1c.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("feedback-form");console.log(e);
+//# sourceMappingURL=03-feedback.d4c36b1c.js.map
---
+++ 03-feedback.fd85c37c.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("feedback-form");console.log(e)}();
+//# sourceMappingURL=03-feedback.fd85c37c.js.map
